91f87624d056aea6ec1142f1b2915e7ca2bbe262
Alexandre Nicastro <alexandre.nicastro@nikas.org>
2022-10-10 04:28:18 +0000

    feat: add Primer Design System
    Signed-off-by: GitHub <noreply@github.com>

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
+  // swcMinify: true,
   poweredByHeader: false,
   // experimental: { images: { layoutRaw: true } },
 };
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import React from 'react'
+import {ThemeProvider, BaseStyles} from '@primer/react'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function App({ Component, pageProps }) {
+  return (
+    <ThemeProvider>
+      <BaseStyles>
+        <Component {...pageProps} />
+      </BaseStyles>
+    </ThemeProvider>
+  );
 }
 
-export default MyApp
+export default App;
