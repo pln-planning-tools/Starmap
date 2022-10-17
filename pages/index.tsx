9e0d2e9d23f58a28aaace70d328aeba0176d639d
Alexandre Nicastro <alexandre.nicastro@nikas.org>
2022-10-17 08:46:58 -0300

    fix(pages): remove functions that are causing errors
    Signed-off-by: Alexandre Nicastro <alexandre.nicastro@nikas.org>

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,111 +5,111 @@ import { Box, Container, Grid, Input, Text, Link } from '@chakra-ui/react';
 import React, { useEffect, useState } from 'react';
 // import Link from 'next/link';
 
-export const getStaticProps: GetStaticProps = async () => {
-  const res = await fetch(
-    `${addHttpsIfNotLocal(
-      process.env.NEXT_PUBLIC_VERCEL_URL,
-    )}/api/github-issue?depth=1&url=https://github.com/pln-roadmap/tests/issues/9`,
-  );
-  const issueData = await res.json();
+// export const getStaticProps: GetStaticProps = async () => {
+//   const res = await fetch(
+//     `${addHttpsIfNotLocal(
+//       process.env.NEXT_PUBLIC_VERCEL_URL,
+//     )}/api/github-issue?depth=1&url=https://github.com/pln-roadmap/tests/issues/9`,
+//   );
+//   const issueData = await res.json();
 
-  // const transformData = (v) => {
-  //   return v.lists
-  //     .flatMap((v) => v.childrenIssues)
-  //     .map((v) => ({
-  //       ...v,
-  //       dueDate: new Date(v.dueDate),
-  //     }));
-  // };
+//   // const transformData = (v) => {
+//   //   return v.lists
+//   //     .flatMap((v) => v.childrenIssues)
+//   //     .map((v) => ({
+//   //       ...v,
+//   //       dueDate: new Date(v.dueDate),
+//   //     }));
+//   // };
 
-  return {
-    props: {
-      issueData,
-    },
-  };
-};
+//   return {
+//     props: {
+//       issueData,
+//     },
+//   };
+// };
 
-const Item = ({ data }) => {
-  console.log('data', data);
-  return (
-    <div
-      style={{
-        // gridColumn: `${data.gridRow} / span 6`,
-        gridColumn: '1 / span 6',
-        padding: '10px 20px',
-        borderRadius: '2px',
-        background: '#e2e2e2',
-        gridRow: data.gridRow,
-      }}
-    >
-      <div>
-        <span>{data.title}</span>
+// const Item = ({ data }) => {
+//   console.log('data', data);
+//   return (
+//     <div
+//       style={{
+//         // gridColumn: `${data.gridRow} / span 6`,
+//         gridColumn: '1 / span 6',
+//         padding: '10px 20px',
+//         borderRadius: '2px',
+//         background: '#e2e2e2',
+//         gridRow: data.gridRow,
+//       }}
+//     >
+//       <div>
+//         <span>{data.title}</span>
 
-        <p style={{ marginTop: '4px', fontSize: '14px' }}>{`dueDate: ${data.dueDate}`}</p>
-        {/* {'<date-end>'} */}
-      </div>
-    </div>
-  );
-};
+//         <p style={{ marginTop: '4px', fontSize: '14px' }}>{`dueDate: ${data.dueDate}`}</p>
+//         {/* {'<date-end>'} */}
+//       </div>
+//     </div>
+//   );
+// };
 
-const RoadmapForm = () => {
-  const [value, setValue] = useState('');
-  const [currentIssueUrl, setCurrentIssueUrl] = useState();
-  const [issueUrl, setIssueUrl] = useState();
-  const [error, setError] = useState();
-  const [issueData, setIssueData] = useState();
-  const [isLoading, setLoading] = useState(false);
-  const getCurrentUrl = () => currentIssueUrl;
+// const RoadmapForm = () => {
+//   const [value, setValue] = useState('');
+//   const [currentIssueUrl, setCurrentIssueUrl] = useState();
+//   const [issueUrl, setIssueUrl] = useState();
+//   const [error, setError] = useState();
+//   const [issueData, setIssueData] = useState();
+//   const [isLoading, setLoading] = useState(false);
+//   const getCurrentUrl = () => currentIssueUrl;
 
-  useEffect(() => {
-    if (!issueUrl) return;
-    setLoading(true);
-    fetch(`/api/github-issue?depth=1&url=${new URL(issueUrl)}`)
-      .then((res) => {
-        console.log('inside fetch!');
-        return res.json();
-      })
-      .then((resData) => {
-        console.log('inside data!', resData);
-        setIssueData(resData);
-        // setIssueUrl(null as any);
-        setLoading(false);
-      })
-      .catch((err) => `error: ${err}`);
-  }, [issueUrl]);
+//   useEffect(() => {
+//     if (!issueUrl) return;
+//     setLoading(true);
+//     fetch(`/api/github-issue?depth=1&url=${new URL(issueUrl)}`)
+//       .then((res) => {
+//         console.log('inside fetch!');
+//         return res.json();
+//       })
+//       .then((resData) => {
+//         console.log('inside data!', resData);
+//         setIssueData(resData);
+//         // setIssueUrl(null as any);
+//         setLoading(false);
+//       })
+//       .catch((err) => `error: ${err}`);
+//   }, [issueUrl]);
 
-  return (
-    <>
-      <form
-        onSubmit={(e) => {
-          e.preventDefault();
-          try {
-            // @ts-ignore
-            const newUrl = new URL(getCurrentUrl());
-            // @ts-ignore
-            setIssueUrl(newUrl.toString());
-          } catch (err: any) {
-            setError(err);
-          }
-        }}
-      >
-        <Text mb='8px'>GitHub URL: {issueUrl}</Text>
-        <Input
-          aria-label='Issue URL'
-          name='issue-url'
-          autoComplete='url'
-          onChange={(e) => setCurrentIssueUrl(e.target.value as any)}
-          placeholder='https://github.com/...'
-          size='sm'
-        />
-      </form>
-    </>
-  );
-};
+//   return (
+//     <>
+//       <form
+//         onSubmit={(e) => {
+//           e.preventDefault();
+//           try {
+//             // @ts-ignore
+//             const newUrl = new URL(getCurrentUrl());
+//             // @ts-ignore
+//             setIssueUrl(newUrl.toString());
+//           } catch (err: any) {
+//             setError(err);
+//           }
+//         }}
+//       >
+//         <Text mb='8px'>GitHub URL: {issueUrl}</Text>
+//         <Input
+//           aria-label='Issue URL'
+//           name='issue-url'
+//           autoComplete='url'
+//           onChange={(e) => setCurrentIssueUrl(e.target.value as any)}
+//           placeholder='https://github.com/...'
+//           size='sm'
+//         />
+//       </form>
+//     </>
+//   );
+// };
 
-const RoadmapApp: NextPage = (pageProps: InferGetStaticPropsType<typeof getStaticProps>) => {
+const RoadmapApp: NextPage = () => {
   // console.dir(pageProps, { depth: Infinity, maxArrayLength: Infinity });
-  const milestones = pageProps?.issueData?.lists?.flatMap((v) => v.childrenIssues);
+  // const milestones = pageProps?.issueData?.lists?.flatMap((v) => v.childrenIssues);
   // console.dir(milestones, { depth: Infinity, maxArrayLength: Infinity });
   const [value, setValue] = useState('');
   const [currentIssueUrl, setCurrentIssueUrl] = useState();
