93d8201e63d9a38c5d2c8640e3980c7001e642e3
Alexandre Nicastro <alexandre.nicastro@nikas.org>
2022-10-19 20:21:07 -0300

    feat: new parser, general refactor & cleanup
    Signed-off-by: Alexandre Nicastro <alexandre.nicastro@nikas.org>

--- lib/parser.ts
+++ lib/parser.ts
@@ -1,62 +1,12 @@
 import { parseHTML } from 'linkedom';
-
-export const parseIssue = (issue) => {
-  // console.log('issue ->', issue);
-  const { document } = parseHTML(issue);
-
-  const lists = [...document.querySelectorAll('ul')]
-    .reduce((a: any, b) => {
-      const listItem = Object.create({});
-      listItem.title = b.previousElementSibling?.textContent?.trim();
-      // Get the title only if the heading is level 3
-      // listItem.title = b.previousElementSibling?.tagName == 'h3' && b.previousElementSibling?.textContent?.trim();
-      // @ts-ignore
-      listItem.childrenIssues = [...b.querySelectorAll('a[href][data-hovercard-type*="issue"]')]?.map((v) => v.href);
-
-      return [...a, listItem];
-    }, [])
-    .filter((v) => v.childrenIssues.length > 0);
-
-  // console.log(lists);
-
-  // nodeType 3 == Text Node
-  const config =
-    (document.querySelector('p:first-child') &&
-      document.querySelector('p:first-child')?.childNodes &&
-      Object.fromEntries(
-        // @ts-ignore
-        [...document.querySelector('p:first-child').childNodes]
-          .filter((v) => v.nodeType === 3)
-          .map((v) => v.textContent?.trim().split(':')),
-      )) ||
-    {};
-
-  // console.log('config ->', config);
-
-  return {
-    config,
-    lists,
-  };
-};
+import { getEtaDate } from '../utils/regexes';
 
 export const getConfig = (issue) => {
   const { document } = parseHTML(issue);
-  return (
-    (document.querySelector('p:first-child') &&
-      document.querySelector('p:first-child')?.childNodes &&
-      Object.fromEntries(
-        // @ts-ignore
-        [...document.querySelector('p:first-child').childNodes]
-          .filter((v) => v.nodeType === 3)
-          .map((v) =>
-            v.textContent
-              ?.trim()
-              .split(':')
-              .map((v) => v.trim()),
-          ),
-      )) ||
-    {}
-  );
+  const issueText = [...document.querySelectorAll('*')].map((v) => v.textContent).join('\n');
+  return {
+    eta: getEtaDate(issueText),
+  };
 };
 
 export const getLists = (issue) => {
@@ -67,8 +17,9 @@ export const getLists = (issue) => {
       listItem.title = b.previousElementSibling?.textContent?.trim();
       // Get the title only if the heading is level 3
       // listItem.title = b.previousElementSibling?.tagName == 'h3' && b.previousElementSibling?.textContent?.trim();
-      // @ts-ignore
-      listItem.childrenIssues = [...b.querySelectorAll('a[href][data-hovercard-type*="issue"]')]?.map((v) => v.href);
+      listItem.childrenIssues = [...b.querySelectorAll('a[href][data-hovercard-type*="issue"]')]?.map(
+        (v: any) => v.href,
+      );
 
       return [...a, listItem];
     }, [])
