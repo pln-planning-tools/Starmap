1a12bb40e838997670a08ca724b98934c8166723
Russell Dempsey <1173416+SgtPooki@users.noreply.github.com>
2022-10-20 13:52:21 -0700

    chore: cleanup useWeekTicks

--- hooks/useWeekTicks.ts
+++ hooks/useWeekTicks.ts
@@ -1,21 +1,6 @@
-import { Dispatch, SetStateAction, useEffect, useState } from 'react';
+import { useState } from 'react';
 import useSharedHook from '../lib/client/createSharedHook';
 
-const [useWeekTicks, setWeekTicks] = useSharedHook(useState, 2)
-// function useWeekTicks(defaultValue?: number): [number, Dispatch<SetStateAction<number>>] {
-//   const [numWeeks, setWeekTicks] = useState(2);
-//   // const [state, setState] = useState({
-//   //   value: defaultValue,
-//   //   error: null,
-//   //   isPending: true
-//   // });
-//   useEffect(() => {
-//     console.log('useWeekTicks numWeeks', numWeeks)
-//   }, [numWeeks]);
-
-
-//   // const { value, error, isPending } = state;
-//   return [numWeeks, setNumWeeks];
-// }
+const [useWeekTicks, setWeekTicks] = useSharedHook(useState, 4)
 
 export { useWeekTicks, setWeekTicks };
