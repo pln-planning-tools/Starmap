4b0ae7918dc6edb9d6c99b2bca9c4087bbe66a5a
Russell Dempsey <1173416+SgtPooki@users.noreply.github.com>
2022-11-11 13:50:17 -0800

    feat: render User Guide.md on landing page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,34 @@
+import {readFile} from 'fs/promises'
+import {join} from 'path'
+
 import type { NextPage } from 'next';
 import React from 'react';
+import ReactMarkdown from 'react-markdown'
+import remarkGfm from 'remark-gfm'
+import ChakraUIRenderer from 'chakra-ui-markdown-renderer';
 
 import PageHeader from '../components/layout/PageHeader';
 
-const App: NextPage = () => {
-  console.log();
+interface SSProps {
+  markdown: string
+}
 
+export async function getServerSideProps(context): Promise<{props: SSProps}> {
+  const filePath = join(process.cwd(), 'User Guide.md');
+  const markdown = await readFile(filePath, 'utf8');
+
+  return {
+    props: {
+      markdown
+    },
+  };
+}
+
+const App: NextPage<SSProps> = ({markdown}: SSProps) => {
   return (
     <>
       <PageHeader />
+      <ReactMarkdown components={ChakraUIRenderer()} children={markdown} remarkPlugins={[remarkGfm]} />
     </>
   );
 };
