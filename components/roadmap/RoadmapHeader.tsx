15f1a79cda3f64b16b3c5bd657e5a962369840f9
Russell Dempsey <1173416+SgtPooki@users.noreply.github.com>
2022-11-14 13:17:56 -0800

    Revert "chore: cleanup unused code"
    This reverts commit efdb271aeb478e6d3466c3b1260a3549f76dec8d.

--- components/roadmap/RoadmapHeader.tsx
+++ components/roadmap/RoadmapHeader.tsx
@@ -1,6 +1,9 @@
+import NextLink from 'next/link';
 import { useRouter } from 'next/router';
-import { Link, Text } from '@chakra-ui/react';
 
+import { Box, Link, Progress, Text } from '@chakra-ui/react';
+
+import { getLinkForRoadmapChild } from '../../lib/client/linkUtils';
 import { IssueData } from '../../lib/types';
 
 interface RoadmapHeaderProps {
@@ -8,16 +11,28 @@ interface RoadmapHeaderProps {
 }
 
 function RoadmapHeader({ issueData }: RoadmapHeaderProps) {
+  // console.log(`issueData: `, issueData);
   const router = useRouter();
 
   return (
     <>
+      {/* TODO: */}
       <Text onClick={() => router.back()}>
         <Link color='blue.500'>{'<'} Back to parent roadmap</Link>
       </Text>
+      {/* TODO: add github icon */}
       <Text as='span' mb='8px' fontSize={40} fontWeight={600}>
+        {/* <NextLink href={getLinkForRoadmapChild(issueData)} passHref> */}
+        {/* <Link color='black.500' > */}
         {issueData.title}
+        {/* </Link> */}
+        {/* </NextLink> */}
       </Text>
+      {/* <Text as="span" mb='8px' fontSize={10} fontWeight={600}>
+        <NextLink href={issueData.html_url} passHref>
+          <Link color='blue.500'>(github)</Link>
+        </NextLink>
+      </Text> */}
     </>
   );
 }
