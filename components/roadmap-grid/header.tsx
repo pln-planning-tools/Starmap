92d36d229b62955991abaaf68cc59c710772f2ec
Russell Dempsey <1173416+SgtPooki@users.noreply.github.com>
2022-11-14 13:16:42 -0800

    feat: Add link to GitHub for each page

--- components/roadmap-grid/header.tsx
+++ components/roadmap-grid/header.tsx
@@ -1,7 +1,10 @@
 import { useRouter } from 'next/router';
+import Image from 'next/image';
+import { Link, Text, Flex, Spacer, Center } from '@chakra-ui/react';
+import NextLink from 'next/link'
 
-import { Link, Text } from '@chakra-ui/react';
-
+import themes from '../theme/constants';
+import GitHubSvgIcon from '../icons/iconmonstr-github.svg';
 import { IssueData } from '../../lib/types';
 
 export default function Header({ issueData }: { issueData: IssueData }) {
@@ -11,7 +14,7 @@ export default function Header({ issueData }: { issueData: IssueData }) {
 
   return (
     <>
-      <Text as='span' mb='8px' fontSize={14}>
+      <Text mb='8px' fontSize={14}>
         <Link
           color='blue.500'
           onClick={() => {
@@ -24,10 +27,22 @@ export default function Header({ issueData }: { issueData: IssueData }) {
           Switch to {changeToView} view
         </Link>
       </Text>
-      <br />
-      <Text as='span' mb='8px' fontSize={40} fontWeight={600}>
-        {issueData.title}
-      </Text>
+      <Flex direction={'row'}>
+        <Text as='span' mb='8px' fontSize={40} fontWeight={600}>
+          {issueData.title}
+        </Text>
+        <Spacer />
+        <Center>
+          <NextLink style={{display: 'span'}} passHref href={issueData.html_url}>
+            <Link target="_blank" rel="noopener noreferrer">
+              <Center>
+                <Text as='span' fontSize={15} fontWeight={400} color={themes.light.text.color} pr="0.5rem">View in GitHub</Text>
+                <Image src={GitHubSvgIcon} alt="GitHub Logo" color={themes.light.text.color} width={24} height={24} />
+              </Center>
+            </Link>
+          </NextLink>
+        </Center>
+      </Flex>
     </>
   );
 }
