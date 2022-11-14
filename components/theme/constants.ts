8f619efa429b20668d203bb70bf24804cd216895
Russell Dempsey <1173416+SgtPooki@users.noreply.github.com>
2022-11-14 22:20:28 +0000

    feat: Add link to GitHub for each page (#86)
    * chore: cleanup unused code
    
    * feat: Add link to GitHub for each page
    
    * Revert "chore: cleanup unused code"
    
    This reverts commit efdb271aeb478e6d3466c3b1260a3549f76dec8d.
    
    * feat: use official github logo
    
    Co-authored-by: Nishant Arora <1895906+whizzzkid@users.noreply.github.com>

---
+++ components/theme/constants.ts
@@ -0,0 +1,16 @@
+const lightMode = {
+  text: {
+    color: '#313239'
+  }
+}
+
+const darkMode = {
+  text: {
+    color: '#D7D7D7'
+  }
+}
+
+export default {
+  light: lightMode,
+  dark: darkMode
+}
