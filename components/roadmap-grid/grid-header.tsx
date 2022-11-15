f7592f8b3b7988238338a3f01669fa7370d9ff34
Russell Dempsey <1173416+SgtPooki@users.noreply.github.com>
2022-11-15 03:31:18 +0000

    feat: use quarters as labels instead of dates (#88)
    * chore: add dayjs quarterofyear plugin
    
    see https://day.js.org/docs/en/plugin/quarter-of-year
    
    * feat: use Quarter identifier for labels

--- components/roadmap-grid/grid-header.tsx
+++ components/roadmap-grid/grid-header.tsx
@@ -1,11 +1,16 @@
 import { dayjs } from '../../lib/client/dayjs';
+
 import styles from './Roadmap.module.css';
 
 export function GridHeader({ ticks, index }) {
+  const date = dayjs(ticks).utc();
+  const quarterNum = date.quarter();
+  const year = date.year().toString().slice(2);
+
   return (
     <>
       <div key={index} className={`${styles.item} ${styles.itemHeader}`}>
-        <span>{`${dayjs(ticks).utc().format('MMM DD, YYYY')}`}</span>
+        <span>{`Q${quarterNum} '${year}`}</span>
       </div>
     </>
   );
