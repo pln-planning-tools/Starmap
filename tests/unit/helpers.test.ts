42c66d3fd636ecdf74d480e27cd6c93542fe04ba
Nishant Arora <1895906+whizzzkid@users.noreply.github.com>
2022-11-14 23:00:42 -0700

    Fixed

--- tests/unit/helpers.test.ts
+++ tests/unit/helpers.test.ts
@@ -11,7 +11,7 @@ describe('getEtaDate', () => {
   it('should return a date string for quarters and optional years', () => {
     // quarters
     expect(getEtaDate('eta: 2022Q4')).toBe('2022-12-31');
-    expect(getEtaDate('eta : 2024Q3')).toBe('2024-09-30');
+    expect(getEtaDate('eta: 2024Q3')).toBe('2024-09-30');
   });
 
   it('should return a date string for months and optional years', () => {
@@ -20,9 +20,9 @@ describe('getEtaDate', () => {
     expect(getEtaDate('eta: 2024-07')).toBe('2024-07-31');
   });
 
-  it('should return an empty string', () => {
+  it('should throw error when date is malformed.', () => {
     // invalid dates
-    expect(getEtaDate('eta: 14-11-2022')).toBe(''); // dd-mm-yyyy
-    expect(getEtaDate('eta: ')).toBe('');
+    expect(() => getEtaDate('eta: 14-11-2022')).toThrow(); // dd-mm-yyyy
+    expect(() => getEtaDate('eta: ')).toThrow(); // empty
   });
 });
