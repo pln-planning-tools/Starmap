36fe687c9308e60833e3bbd06fbd5afaa799be75
Nishant Arora <1895906+whizzzkid@users.noreply.github.com>
2022-11-15 01:49:16 -0700

    Fixing exports

--- config/constants.ts
+++ config/constants.ts
@@ -1,9 +1,8 @@
 const BASE_PROTOCOL = (!!process.env.IS_LOCAL && 'http') || 'https';
 const BASE_URL = process.env.NEXT_PUBLIC_VERCEL_URL;
-const API_URL = new URL(`${BASE_PROTOCOL}://${BASE_URL}/api/roadmap`);
+export const API_URL = new URL(`${BASE_PROTOCOL}://${BASE_URL}/api/roadmap`);
 
-const TEN_DAYS_IN_SECONDS = 864000;
-const OFFSET_MIN_MONTHS = 2;
-const OFFSET_MAX_MONTHS = 3;
-
-export { API_URL, TEN_DAYS_IN_SECONDS, OFFSET_MIN_MONTHS, OFFSET_MAX_MONTHS };
+export const TEN_DAYS_IN_SECONDS = 864000;
+export const OFFSET_MIN_MONTHS = 2;
+export const OFFSET_MAX_MONTHS = 3;
+export const DEFAULT_TICK_COUNT = 5;
