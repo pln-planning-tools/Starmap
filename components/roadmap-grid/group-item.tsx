d605f8b9a0ca8185432889507a5199306db0bfed
Russell Dempsey <1173416+SgtPooki@users.noreply.github.com>
2022-11-16 11:35:22 -0800

    fix: group title text coloring

--- components/roadmap-grid/group-item.tsx
+++ components/roadmap-grid/group-item.tsx
@@ -1,25 +1,32 @@
 import { Text } from '@chakra-ui/react';
 import NextLink from 'next/link';
+import { useViewMode } from '../../hooks/useViewMode';
+import { ViewMode } from '../../lib/enums';
 
 import { GroupItemProps } from '../../lib/types';
 import styles from './Roadmap.module.css';
-
-
 
 /**
  * This is the component for the Group header (themes)
  * @returns {JSX.Element}
  */
-export function GroupItem({ showGroupRowTitle, issueData, group }: GroupItemProps) {
-  console.log(`issueData: `, issueData);
-  console.log(`group: `, group);
-  let detailedViewClass = 'detailedView';
-  if (!showGroupRowTitle) {
-    detailedViewClass = '';
+export function GroupItem({issueData, group }: GroupItemProps) {
+  const viewMode = useViewMode();
+  let detailedViewClass = '';
+  let groupNameElement: JSX.Element | null = null;
+
+  if (viewMode === ViewMode.Detail) {
+    detailedViewClass = 'detailedView';
+    if (group.url !== '') {
+      groupNameElement = <Text color="black">{group.groupName}</Text>
+    } else {
+      groupNameElement = <NextLink href={group.url}>{group.groupName}</NextLink>
+    }
   }
+
   return (
     <div className={`${styles.item} ${styles.group}`}>
-      <div>{!!showGroupRowTitle && <NextLink href={group.url}>{group.groupName}</NextLink>}</div>
+      <div>{groupNameElement}</div>
     </div>
   );
 }
