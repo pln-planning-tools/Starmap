1d0be4bbc505fbd925b40ae76647babfed7e2af4
Russell Dempsey <1173416+SgtPooki@users.noreply.github.com>
2022-11-16 12:06:18 -0800

    feat: cache detail/simple view

--- hooks/useViewMode.ts
+++ hooks/useViewMode.ts
@@ -1,8 +1,34 @@
-import { useState } from 'react';
+import { Dispatch, SetStateAction, useEffect, useState } from 'react';
+import { createLocalStorage } from 'localstorage-ponyfill';
 
 import useSharedHook from '../lib/client/createSharedHook';
 import { ViewMode } from '../lib/enums';
 
-const [useViewMode, setViewMode] = useSharedHook(useState, ViewMode.Simple);
+const LOCAL_STORAGE_CACHE_KEY = 'useViewModeCache'
+const customStateFunction: typeof useState = <S = typeof ViewMode>(initialState?: S) => {
+  const localStorage = createLocalStorage();
+  const [state, setState] = useState(initialState);
+
+  useEffect(() => {
+    const cachedValue = localStorage.getItem(LOCAL_STORAGE_CACHE_KEY);
+    const actualCachedValue: S | null = cachedValue ? JSON.parse(cachedValue) : null;
+    if (actualCachedValue != null && actualCachedValue !== state) {
+      setState(actualCachedValue);
+    }
+
+  }, [state, setState, localStorage]);
+
+  const setCachedState = (newState: S) => {
+    localStorage.setItem(LOCAL_STORAGE_CACHE_KEY, JSON.stringify(newState));
+    setState(newState);
+  };
+
+  if (typeof initialState === 'undefined') {
+    return [state, setCachedState] as [S, Dispatch<SetStateAction<S>>];
+  }
+  return [state, setCachedState] as [S | undefined, Dispatch<SetStateAction<S | undefined>>];
+}
+
+const [useViewMode, setViewMode] = useSharedHook(customStateFunction, ViewMode.Simple);
 
 export { useViewMode, setViewMode };
