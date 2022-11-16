dc5f58020c8a20b5ab14de9ec2e4d8e94e3b7809
Russell Dempsey <1173416+SgtPooki@users.noreply.github.com>
2022-11-16 15:41:48 -0800

    fix: group header color

--- components/roadmap-grid/grid-header.tsx
+++ components/roadmap-grid/grid-header.tsx
@@ -2,6 +2,11 @@ import { dayjs } from '../../lib/client/dayjs';
 
 import styles from './Roadmap.module.css';
 
+/**
+ * This is the labels for the grid. The quarters and the top with the ticks.
+ * @param param0
+ * @returns
+ */
 export function GridHeader({ ticks, index }) {
   const date = dayjs(ticks).utc();
   const quarterNum = date.quarter();
