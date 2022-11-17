b51c252112adb65949d75b78b6b543fe6030772f
Nishant Arora <1895906+whizzzkid@users.noreply.github.com>
2022-11-17 16:19:53 -0700

    better throw

--- lib/paramsFromUrl.ts
+++ lib/paramsFromUrl.ts
@@ -2,15 +2,14 @@ import { getValidUrlFromInput } from './getValidUrlFromInput';
 import { slugsFromUrl } from './slugsFromUrl';
 import { UrlMatchSlugs } from './types';
 
-export const paramsFromUrl = (urlString: string): UrlMatchSlugs => {
-
+export function paramsFromUrl (urlString: string): UrlMatchSlugs | undefined {
   try {
     const urlObj = getValidUrlFromInput(urlString)
     const matchResult = slugsFromUrl(urlObj.pathname)
     if (matchResult !== false && matchResult.params != null) {
       return { ...matchResult.params };
     }
-  } catch { }
-
-  throw new Error('Unsupported URL');
+  } catch {
+    throw new Error('Unsupported URL');
+  }
 };
