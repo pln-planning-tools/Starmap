9356f163ea21a720f4d2417a6a7cc202a95938cf
Nishant Arora <1895906+whizzzkid@users.noreply.github.com>
2022-11-17 16:18:22 -0700

    simplify

--- lib/slugsFromUrl.ts
+++ lib/slugsFromUrl.ts
@@ -2,10 +2,7 @@ import { match, MatchResult } from 'path-to-regexp';
 
 import { UrlMatchSlugs } from './types';
 
-export const slugsFromUrl = (url: string): MatchResult<UrlMatchSlugs> | false => {
-  const matchResult = match<UrlMatchSlugs>('/:owner/:repo/issues/:issue_number(\\d+)', {
+export const slugsFromUrl = (url: string): MatchResult<UrlMatchSlugs> | false => match<UrlMatchSlugs>(
+  '/:owner/:repo/issues/:issue_number(\\d+)', {
     decode: decodeURIComponent,
   })(url);
-
-  return matchResult;
-};
