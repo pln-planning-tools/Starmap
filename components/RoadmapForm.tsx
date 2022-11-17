b3c308f1af31a60392e1e4ac6d5565eb8a1eb99f
Russell Dempsey <1173416+SgtPooki@users.noreply.github.com>
2022-11-17 00:13:27 -0800

    feat: much better url error handling

--- components/RoadmapForm.tsx
+++ components/RoadmapForm.tsx
@@ -1,6 +1,6 @@
 import { useRouter } from 'next/router';
-import { Box, Input, InputGroup, InputLeftElement, InputRightElement, Spinner, Text } from '@chakra-ui/react';
-import { ArrowForwardIcon, SearchIcon } from '@chakra-ui/icons'
+import { Button, FormControl, FormErrorMessage, Input, InputGroup, InputLeftElement, InputRightElement, Spinner, Text } from '@chakra-ui/react';
+import { SearchIcon } from '@chakra-ui/icons'
 import { match } from 'path-to-regexp';
 import { useEffect, useState } from 'react';
 
@@ -9,6 +9,8 @@ import styles from './RoadmapForm.module.css'
 import theme from './theme/constants'
 import { setCurrentIssueUrl, useCurrentIssueUrl } from '../hooks/useCurrentIssueUrl';
 import { isEmpty } from 'lodash';
+import { paramsFromUrl } from '../lib/paramsFromUrl';
+import { getValidUrlFromInput } from '../lib/getValidUrlFromInput';
 
 const slugsFromUrl: any = (url) => {
   const matchResult = match('/:owner/:repo/issues/:issue_number(\\d+)', {
@@ -23,63 +25,66 @@ export function RoadmapForm() {
   const isLoading = useIsLoading();
   const currentIssueUrl = useCurrentIssueUrl();
   const [issueUrl, setIssueUrl] = useState<string | null>();
-  const [error, setError] = useState();
+  const [error, setError] = useState<Error | null>(null);
 
   useEffect(() => {
-    if (isEmpty(currentIssueUrl)) {
-      const urlMatchParams = slugsFromUrl(window.location.pathname.replace('/roadmap/github.com', ''))?.params
-      if (urlMatchParams != null) {
-        const { owner, repo, issue_number } = urlMatchParams;
-        setCurrentIssueUrl(`https://github.com/${owner}/${repo}/issues/${issue_number}`);
-      }
-
+    if (isEmpty(currentIssueUrl) && window.location.pathname.length > 1) {
+      try {
+        const urlObj = getValidUrlFromInput(window.location.pathname.replace('/roadmap', ''));
+        setCurrentIssueUrl(urlObj.toString());
+      } catch {}
     }
-  }, [currentIssueUrl, slugsFromUrl])
+  }, [currentIssueUrl, getValidUrlFromInput, setCurrentIssueUrl])
 
   useEffect(() => {
     if (router.isReady) {
       if (!issueUrl) return;
-      const { owner, repo, issue_number } = slugsFromUrl(new URL(issueUrl).pathname).params;
-      setIssueUrl(null);
-      router.push(`/roadmap/github.com/${owner}/${repo}/issues/${issue_number}`).then(() => setIsLoading(false));
+      try {
+        const { owner, repo, issue_number } = paramsFromUrl(issueUrl);
+        setIssueUrl(null);
+        router.push(`/roadmap/github.com/${owner}/${repo}/issues/${issue_number}`).then(() => setIsLoading(false));
+      } catch (err){
+        setError(err as Error);
+        setIsLoading(false);
+      }
     }
   }, [router, issueUrl, setCurrentIssueUrl]);
 
   const formSubmit = (e) => {
     e.preventDefault();
     setIsLoading(true);
+    setError(null);
 
     try {
       if (currentIssueUrl == null) {
         throw new Error('currentIssueUrl is null');
       }
-      const newUrl = new URL(currentIssueUrl);
+      const newUrl = getValidUrlFromInput(currentIssueUrl);
       setIssueUrl(newUrl.toString());
-    } catch (err: any) {
-      setError(err);
+    } catch (err) {
+      setError(err as Error);
       setIsLoading(false);
     }
   }
 
   let inputRightElement = (
-    <Box className={styles.formSubmitButton} border="1px solid #8D8D8D" borderRadius="4px"  bg="rgba(141, 141, 141, 0.3)" onClick={formSubmit}>
+    <Button type="submit" isLoading={isLoading} className={styles.formSubmitButton} border="1px solid #8D8D8D" borderRadius="4px"  bg="rgba(141, 141, 141, 0.3)" onClick={formSubmit}>
       <Text p="6px 10px" color="white">⏎</Text>
-    </Box>
+    </Button>
   )
   if (isLoading) {
     inputRightElement = <Spinner />
   }
   return (
-    <>
-      <form
-        onSubmit={formSubmit}
-      >
+    <form onSubmit={formSubmit}>
+      <FormControl isInvalid={error != null}>
         <InputGroup>
           <InputLeftElement
             pointerEvents='none'
             children={<SearchIcon color='#FFFFFF' />}
           />
           <Input
+            type="text"
             value={currentIssueUrl}
             className={styles.urlInput}
             color={theme.light.header.input.text.color}
@@ -94,7 +99,8 @@ export function RoadmapForm() {
           />
           <InputRightElement cursor="pointer" children={inputRightElement}/>
         </InputGroup>
-      </form>
-    </>
+        <FormErrorMessage>{error?.message}</FormErrorMessage>
+      </FormControl>
+    </form>
   );
 }
