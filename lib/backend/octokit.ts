25a1f1d04b57a8689613196591c8791705a1885d
Russell Dempsey <1173416+SgtPooki@users.noreply.github.com>
2022-11-21 15:43:36 -0800

    fix: fail early on API rate limit exhaustion

--- lib/backend/octokit.ts
+++ lib/backend/octokit.ts
@@ -7,24 +7,39 @@ if (!auth) {
   throw new Error('PLN_ADMIN_GITHUB_TOKEN environmental variable not set. It is required.');
 }
 
+const maxRetryAfterMinutes = 1;
 const RetryableOctokit = Octokit.plugin(retry, throttling);
 
 const octokit = new RetryableOctokit({
   auth,
   throttle: {
-    onRateLimit: (retryAfter, options, octokit) => {
+    onRateLimit: (retryAfter, options, octokit, retryCount) => {
       octokit.log.warn(`Request quota exhausted for request ${options.method} ${options.url}`);
 
-      // retry forever
-      octokit.log.info(`Retrying after ${retryAfter} seconds!`);
-      return true;
+      if (retryAfter / 60 > maxRetryAfterMinutes) {
+        throw new Error(`RetryAfter is over ${maxRetryAfterMinutes} minutes. Aborting.`);
+      }
+      if (retryCount < 2) {
+        // retry 2 times
+        octokit.log.warn(`Retrying after ${retryAfter} seconds!`);
+        return true;
+      } else {
+        throw new Error('Request quota exceeded after two retries.')
+      }
     },
-    onSecondaryRateLimit: (retryAfter, options, octokit) => {
+    onSecondaryRateLimit: (retryAfter, options, octokit, retryCount) => {
       octokit.log.warn(`SecondaryRateLimit detected for request ${options.method} ${options.url}`);
 
-      // retry forever
-      octokit.log.info(`Retrying after ${retryAfter} seconds!`);
-      return true;
+      if (retryAfter / 60 > maxRetryAfterMinutes) {
+        throw new Error(`RetryAfter is over ${maxRetryAfterMinutes} minutes. Aborting.`);
+      }
+      if (retryCount < 2) {
+        // retry 2 times
+        octokit.log.warn(`Retrying after ${retryAfter} seconds!`);
+        return true;
+      } else {
+        throw new Error('Request quota exceeded after two retries.')
+      }
     },
   },
 });
