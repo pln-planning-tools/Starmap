1d8be69547f397b21f89285c69f8df9802f05b06
Nishant Arora <1895906+whizzzkid@users.noreply.github.com>
2022-11-21 13:41:17 -0700

    simpler persistence

--- hooks/useViewMode.ts
+++ hooks/useViewMode.ts
@@ -4,66 +4,50 @@ import useSharedHook from '../lib/client/createSharedHook';
 import { ViewMode } from '../lib/enums';
 
 const LOCAL_STORAGE_CACHE_KEY = 'useViewModeCache'
+const DEFAULT_INITIAL_VIEW_MODE = ViewMode.Simple;
+
 const customStateFunction: typeof useState = <S = typeof ViewMode>(initialState?: S) => {
 
   const [state, setState] = useState(initialState);
-  /**
-   * We should only access localStorage inside useEffect because otherwise nextjs
-   * will try to use it on the server and it will fail.
-   */
-  const [localStorageValue, setLocalStorageValue] = useState<S | null>(null);
 
   const getCurrentValueFromStorage = () => {
-    const cachedValue = localStorage?.getItem(LOCAL_STORAGE_CACHE_KEY);
-    const actualCachedValue = JSON.parse(cachedValue ?? '""');
-    return actualCachedValue;
-  }
-
-  /**
-   * Update localStorageValue to equal what is in localStorage
-   */
-  useEffect(() => {
-    const actualCachedValue = getCurrentValueFromStorage();
-    if (actualCachedValue !== '' && localStorageValue !== actualCachedValue) {
-      setLocalStorageValue(actualCachedValue);
-    }
-  }, [setLocalStorageValue])
+    const persistedValue = localStorage?.getItem(LOCAL_STORAGE_CACHE_KEY);
+    const actualPersistedValue = JSON.parse(persistedValue ?? '""');
+    return actualPersistedValue;
+  };
 
   /**
    * Update the saved localStorage value to equal the current localStorage State
    * value if the values are in sync.
    */
   useEffect(() => {
-    const actualCachedValue = getCurrentValueFromStorage();
-    if (actualCachedValue !== state && state === localStorageValue) {
-      localStorage?.setItem(LOCAL_STORAGE_CACHE_KEY, JSON.stringify(localStorageValue));
+    const actualPersistedValue = getCurrentValueFromStorage();
+    if (!state) {
+      const initialStateFromStorage = actualPersistedValue === '' ? DEFAULT_INITIAL_VIEW_MODE : actualPersistedValue;
+      setState(initialStateFromStorage);
+    } else {
+      if (actualPersistedValue !== state) {
+        localStorage?.setItem(LOCAL_STORAGE_CACHE_KEY, JSON.stringify(state));
+      }
     }
-  }, [state, localStorageValue])
+  }, [state])
 
   /**
    * Update the actual viewMode value if it is different from what is in localStorage
    */
   useEffect(() => {
-    if (localStorageValue != null && state !== localStorageValue) {
-      console.log(`setting state to`, { localStorageValue, state }),
-      setState(localStorageValue);
+    const actualPersistedValue = getCurrentValueFromStorage();
+    if (actualPersistedValue !== state) {
+      setState(actualPersistedValue);
     }
-  }, [state, setState, localStorageValue]);
-
-  /**
-   * Update both the actual viewMode value and the localStorage value
-   * @param {S} newState - the new value to set
-   */
-  const setCachedState = (newState: S) => {
-    setLocalStorageValue(newState);
-  };
+  }, [state]);
 
   if (typeof initialState === 'undefined') {
-    return [state, setCachedState] as [S, Dispatch<SetStateAction<S>>];
+    return [state, setState] as [S, Dispatch<SetStateAction<S>>];
   }
-  return [state, setCachedState] as [S | undefined, Dispatch<SetStateAction<S | undefined>>];
+  return [state, setState] as [S | undefined, Dispatch<SetStateAction<S | undefined>>];
 }
 
-const [useViewMode, setViewMode] = useSharedHook(customStateFunction, ViewMode.Simple);
+const [useViewMode, setViewMode] = useSharedHook(customStateFunction, undefined);
 
 export { useViewMode, setViewMode };
