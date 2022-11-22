d3954bda837e96311babd5f9c04a1b45741d116c
Russell Dempsey <1173416+SgtPooki@users.noreply.github.com>
2022-11-22 13:25:00 -0800

    fix: test names

--- tests/unit/backend/calculateCompletionRate.test.ts
+++ tests/unit/backend/calculateCompletionRate.test.ts
@@ -13,18 +13,18 @@ describe('calculateCompletionRate', function() {
     it('with 1 closed child to 100', () => {
       expect(calculateCompletionRate({children: [{state: IssueStates.CLOSED}], state: IssueStates.CLOSED})).toEqual(100);
     });
-    it('without valid children to 0', () => {
+    it('without valid children to 100', () => {
       expect(calculateCompletionRate({children: null as unknown as {state: IssueStates}[], state: IssueStates.CLOSED})).toEqual(100);
     });
 
     describe('multiple children', () => {
-      it('with 2 open children', () => {
+      it('with 2 open children to 33.33', () => {
         expect(calculateCompletionRate({children: [{state: IssueStates.OPEN}, {state: IssueStates.OPEN}], state: IssueStates.CLOSED})).toEqual(33.33);
       });
-      it('with 1 open child and 1 closed child', () => {
+      it('with 1 open child and 1 closed child to 66.67', () => {
         expect(calculateCompletionRate({children: [{state: IssueStates.OPEN}, {state: IssueStates.CLOSED}], state: IssueStates.CLOSED})).toEqual(66.67);
       });
-      it('with 2 closed children', () => {
+      it('with 2 closed children to 100', () => {
         expect(calculateCompletionRate({children: [{state: IssueStates.CLOSED}, {state: IssueStates.CLOSED}], state: IssueStates.CLOSED})).toEqual(100);
       });
     });
@@ -34,7 +34,7 @@ describe('calculateCompletionRate', function() {
     it('with no children to 0', () => {
       expect(calculateCompletionRate({children: [], state: IssueStates.OPEN})).toEqual(0);
     });
-    it('with 1 open child to 50', () => {
+    it('with 1 open child to 0', () => {
       expect(calculateCompletionRate({children: [{state: IssueStates.OPEN}], state: IssueStates.OPEN})).toEqual(0);
     });
     it('with 1 closed child to 50', () => {
@@ -45,13 +45,13 @@ describe('calculateCompletionRate', function() {
     });
 
     describe('multiple children', () => {
-      it('with 2 open children', () => {
+      it('with 2 open children to 0', () => {
         expect(calculateCompletionRate({children: [{state: IssueStates.OPEN}, {state: IssueStates.OPEN}], state: IssueStates.OPEN})).toEqual(0);
       });
-      it('with 1 open child and 1 closed child', () => {
+      it('with 1 open child and 1 closed child to 33.33', () => {
         expect(calculateCompletionRate({children: [{state: IssueStates.OPEN}, {state: IssueStates.CLOSED}], state: IssueStates.OPEN})).toEqual(33.33);
       });
-      it('with 2 closed children', () => {
+      it('with 2 closed children to 66.67', () => {
         expect(calculateCompletionRate({children: [{state: IssueStates.CLOSED}, {state: IssueStates.CLOSED}], state: IssueStates.OPEN})).toEqual(66.67);
       });
     });
