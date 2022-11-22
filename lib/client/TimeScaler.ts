5da34f0a59e068e6fe719991b9c2300ca1197389
Russell Dempsey <1173416+SgtPooki@users.noreply.github.com>
2022-11-21 17:54:56 -0800

    feat: fix css-grid milestone accuracy (#152)
    * feat: create global scaleTime hook
    
    * feat: add NumSelector slider component
    
    * tmp
    
    * feat: make header grid-column-end dynamic
    
    * feat: ensure milestone item width is adjustable
    
    * feat: display current value in NumSlider
    
    * chore: remove unused code
    
    * feat: make gridCol changes smoother
    
    * chore: fix accuracy of milestone items
    
    * feat: implement todayLine and nail down milestone accuracy
    
    * feat: add ticksRatio to display more ticks than the ones with headers
    
    * fix: today-line
    
    * chore: polish and more accuracy
    
    * chore: align styles with figma
    
    * chore: minor adjustments to today line
    
    * Update components/roadmap-grid/RoadmapDetailedView.tsx
    
    Co-authored-by: Nishant Arora <1895906+whizzzkid@users.noreply.github.com>
    
    * chore: code cleanup and comments
    
    Co-authored-by: Nishant Arora <1895906+whizzzkid@users.noreply.github.com>

---
+++ lib/client/TimeScaler.ts
@@ -0,0 +1,35 @@
+import { ScaleTime, scaleTime } from 'd3';
+
+import { dayjs } from './dayjs';
+
+/**
+ * TODO: Implement using React Provider pattern
+ */
+export class TimeScaler {
+  gridColScale: ScaleTime<number, number>;
+  percentageScale: ScaleTime<number, number>;
+  constructor() {
+    this.percentageScale = scaleTime();
+    this.gridColScale = scaleTime();
+  }
+
+  setScale(dates: Date[], numCols: number) {
+    const validDates = dates.map(dayjs).filter((d) => d.isValid())
+    const minDate = dayjs.min(validDates);
+    const maxDate = dayjs.max(validDates);
+    const domain = [minDate, maxDate];
+    this.percentageScale = scaleTime().domain(domain).range([0, 1]);
+    this.gridColScale = scaleTime().domain(domain).range([0, numCols]);
+  }
+
+  getPercentile(date: Date) {
+    return this.percentageScale(date)
+  }
+
+  getColumn(date: Date) {
+    return this.gridColScale(date)
+  }
+}
+
+
+export const globalTimeScaler = new TimeScaler();
