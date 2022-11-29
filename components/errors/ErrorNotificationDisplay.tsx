ce0a41d95d14b9fbdb98067d09b462f8095c4101
Russell Dempsey <1173416+SgtPooki@users.noreply.github.com>
2022-11-28 17:14:35 -0800

    Update components/errors/ErrorNotificationDisplay.tsx
    Co-authored-by: Nishant Arora <1895906+whizzzkid@users.noreply.github.com>

--- components/errors/ErrorNotificationDisplay.tsx
+++ components/errors/ErrorNotificationDisplay.tsx
@@ -24,7 +24,7 @@ export function ErrorNotificationDisplay ({errors, issueDataState}: ErrorNotific
     filteredErrors = errorFilters[viewMode](errors, issueDataState.value)
   }
 
-  if (filteredErrors == null || filteredErrors.length === 0) {
+  if (filteredErrors?.length === 0) {
     return null;
   }
 
