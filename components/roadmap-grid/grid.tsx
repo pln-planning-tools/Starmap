5a8e3ab04bdf8be457b72b062968c2a998769be3
Dan McQuillan <djmcquillan76@gmail.com>
2022-11-28 18:02:55 -0600

    fix/175-updates based on pr

--- components/roadmap-grid/grid.tsx
+++ components/roadmap-grid/grid.tsx
@@ -4,7 +4,7 @@ import { TodayMarker } from './today-marker';
 export function Grid({ children, ticksLength, scroll = false, renderTodayLine = false}) {
   const styleClass = scroll ? styles.scrollable : styles.grid;
   return (
-    <div className={styleClass} style={{ gridTemplateColumns: `repeat(${ticksLength}, minmax(10px, 1fr))`, marginTop: '-20px' }}>
+    <div className={styleClass} style={{ gridTemplateColumns: `repeat(${ticksLength}, minmax(10px, 1fr))`}}>
       {renderTodayLine ? <TodayMarker /> : null}
       {children}
     </div>
