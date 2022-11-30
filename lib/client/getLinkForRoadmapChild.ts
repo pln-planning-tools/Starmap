d4df83b8d70083a42c90fbf45a3f493b0bb3c7f6
Russell Dempsey <1173416+SgtPooki@users.noreply.github.com>
2022-11-30 13:56:07 -0800

    fix: cognitive complexity of getLinkForRoadmapChild.ts

--- lib/client/getLinkForRoadmapChild.ts
+++ lib/client/getLinkForRoadmapChild.ts
@@ -7,13 +7,8 @@ interface GetLinkForRoadmapChildOptions {
   query?: {crumbs?: string};
   currentRoadmapRoot?: Pick<IssueData, 'html_url' | 'title'>;
 }
-export function getLinkForRoadmapChild({ issueData, currentRoadmapRoot, query }: GetLinkForRoadmapChildOptions): string {
-  if (issueData == null || issueData?.children?.length === 0) {
-    return '#';
-  }
-  currentRoadmapRoot = currentRoadmapRoot ?? issueData.parent;
-  const urlM = paramsFromUrl(issueData.html_url);
-  const url = new URL(`/roadmap/github.com/${urlM.owner}/${urlM.repo}/issues/${urlM.issue_number}`, window.location.origin);
+
+function addCrumbsParamToUrl({ url, currentRoadmapRoot, query }: Pick<GetLinkForRoadmapChildOptions, 'currentRoadmapRoot' | 'query'> & {url: URL}) {
   if (currentRoadmapRoot != null) {
     const crumbForParent = getCrumbStringFromIssueData(currentRoadmapRoot);
     let crumbString = crumbForParent;
@@ -23,6 +18,16 @@ export function getLinkForRoadmapChild({ issueData, currentRoadmapRoot, query }:
     }
     url.searchParams.set('crumbs', crumbString);
   }
+}
+
+export function getLinkForRoadmapChild({ issueData, currentRoadmapRoot, query }: GetLinkForRoadmapChildOptions): string {
+  if (issueData == null || issueData?.children?.length === 0) {
+    return '#';
+  }
+  currentRoadmapRoot = currentRoadmapRoot ?? issueData.parent;
+  const urlM = paramsFromUrl(issueData.html_url);
+  const url = new URL(`/roadmap/github.com/${urlM.owner}/${urlM.repo}/issues/${urlM.issue_number}`, window.location.origin);
+  addCrumbsParamToUrl({ currentRoadmapRoot, query, url });
 
   return url.toString().replace(window.location.origin, '');
 }
