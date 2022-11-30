a0218d8162b1222ccfaf6254905108e897c0c652
Russell Dempsey <1173416+SgtPooki@users.noreply.github.com>
2022-11-30 15:25:51 -0800

    fix: set global loading to true on breadcrumb click

--- components/breadcrumb/StarmapsBreadcrumbItem.tsx
+++ components/breadcrumb/StarmapsBreadcrumbItem.tsx
@@ -1,15 +1,27 @@
 import { BreadcrumbItem, BreadcrumbItemProps, BreadcrumbLink } from '@chakra-ui/react';
 import React from 'react';
+import { useGlobalLoadingState } from '../../hooks/useGlobalLoadingState';
 
 interface StarmapsBreadcrumbItemProps extends BreadcrumbItemProps {
   title: string;
   url: string | null;
 }
-export function StarmapsBreadcrumbItem({ title, url, ...props }: StarmapsBreadcrumbItemProps) {
 
+export function StarmapsBreadcrumbItem({ title, url, ...props }: StarmapsBreadcrumbItemProps) {
+  const globalLoadingState = useGlobalLoadingState();
+  // eslint-disable-next-line arrow-body-style
+  let onClickHandler = () => {
+    return;
+  }
+  const isClickable = url != null;
+  if (isClickable) {
+    onClickHandler = () => {
+      globalLoadingState.start();
+    }
+  }
   return (
-    <BreadcrumbItem isCurrentPage={url == null} {...props}>
-        <BreadcrumbLink href={url ?? ''} textDecoration={url == null ? 'none' : 'underline'}>
+    <BreadcrumbItem onClick={onClickHandler} className="js-breadcrumbItem-link" isCurrentPage={!isClickable} {...props}>
+        <BreadcrumbLink href={url ?? ''} textDecoration={isClickable ? 'underline' : 'none'}>
             {title}
         </BreadcrumbLink>
     </BreadcrumbItem>
