d4002afa7afeb19d83934e89e40c53c2fa8cae55
Nishant Arora <1895906+whizzzkid@users.noreply.github.com>
2022-11-29 23:46:13 -0700

    rendering line before grid

--- components/roadmap-grid/grid.tsx
+++ components/roadmap-grid/grid.tsx
@@ -4,9 +4,11 @@ import { TodayMarker } from './today-marker';
 export function Grid({ children, ticksLength, scroll = false, renderTodayLine = false }) {
   const styleClass = scroll ? styles.scrollable : styles.grid;
   return (
-    <div className={styleClass} style={{ gridTemplateColumns: `repeat(${ticksLength}, minmax(10px, 1fr))` }}>
+    <>
       {renderTodayLine ? <TodayMarker /> : null}
-      {children}
-    </div>
+      <div className={styleClass} style={{ gridTemplateColumns: `repeat(${ticksLength}, minmax(10px, 1fr))` }}>
+        {children}
+      </div>
+    </>
   );
 }
