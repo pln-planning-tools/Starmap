22c73df7a77ea73ffe1cbbefeaa1ea4abf0375f4
Russell Dempsey <1173416+SgtPooki@users.noreply.github.com>
2022-12-01 01:21:28 -0800

    chore: add prod source maps
    see https://nextjs.org/docs/advanced-features/source-maps

--- next.config.js
+++ next.config.js
@@ -13,6 +13,7 @@ const nextConfig = {
 
 module.exports = {
   ...nextConfig,
+  productionBrowserSourceMaps: true,
 
   webpack(config, options) {
     config.experiments = config.experiments || {};
