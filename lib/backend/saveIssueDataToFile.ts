045f3fb68351a8bb4fd0cfc20a4b2d32f8ab821b
Russell Dempsey <1173416+SgtPooki@users.noreply.github.com>
2022-11-30 16:23:35 -0800

    feat: support saving pendingChild results to filesystem to save on API calls

--- lib/backend/saveIssueDataToFile.ts
+++ lib/backend/saveIssueDataToFile.ts
@@ -1,4 +1,4 @@
-import { writeFile } from 'fs/promises';
+import { writeFile, readFile } from 'fs/promises';
 import { join } from 'path';
 import { IssueData } from '../types';
 import path from 'path';
@@ -12,6 +12,9 @@ const __dirname = path.dirname(__filename);
    * Save the finalIssueData to a file with the owner, repo, and issue number as name
    */
 export function saveIssueDataToFile(finalIssueData: IssueData) {
+  if (process.env.IS_LOCAL !== 'true' || process.env.ENABLE_ISSUEDATA_SAVING !== 'true') {
+    return;
+  }
   /**
    * Get the owner, repo, and issue_number from the issue's html_url using paramsFromUrl
    */
@@ -24,5 +27,23 @@ export function saveIssueDataToFile(finalIssueData: IssueData) {
   writeFile(filePath, JSON.stringify(finalIssueData, null, 2)).catch((err) => {
     console.error(`Error writing file ${filePath}`, err);
   });
+  // throw new Error('Function not implemented.');
+}
+
+export async function checkForSavedIssueData({ owner, repo, issue_number }): Promise<IssueData> {
+  if (process.env.IS_LOCAL !== 'true' || process.env.ENABLE_ISSUEDATA_READING !== 'true') {
+    throw new Error('checkForSavedIssueData not enabled. Check that IS_LOCAL and ENABLE_ISSUEDATA_READING are set to true');
+  }
+
+  const pathToSaveFiles = join(__dirname, '..', '..', 'tests', 'fixtures', 'issueData');
+  const fileName = `${owner}-${repo}-${issue_number}.json`;
+  const filePath = join(pathToSaveFiles, fileName);
+  try {
+    const issueData = await readFile(filePath, 'utf8');
+    return JSON.parse(issueData);
+  } catch (err) {
+    throw new Error(`Error reading file ${filePath}: ${(err as Error).toString()}`);
+  }
+
   // throw new Error('Function not implemented.');
 }
