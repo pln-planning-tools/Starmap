4dd3cdb2f029f842c65409c9c028ed7d4ebd0a50
Russell Dempsey <1173416+SgtPooki@users.noreply.github.com>
2022-12-06 10:04:53 -0800

    Update lib/client/getUniqIdForGroupedIssues.ts
    Co-authored-by: Nishant Arora <1895906+whizzzkid@users.noreply.github.com>

--- lib/client/getUniqIdForGroupedIssues.ts
+++ lib/client/getUniqIdForGroupedIssues.ts
@@ -6,7 +6,7 @@ interface GetUniqIdForGroupedIssuesArgs extends Omit<DetailedViewGroup, 'items'>
 }
 export default function getUniqIdForGroupedIssues (groupedIssues: GetUniqIdForGroupedIssuesArgs[]): string {
   return groupedIssues.map((group) => {
-    const groupChildrenId = group.items.map((child) => child.node_id).join('-')
+    const groupChildrenId = group.items.map(({ node_id }) => node_id).join('-')
     return `${group.groupName}${groupChildrenId}`
   }).join('--')
 }
