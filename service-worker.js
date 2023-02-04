8929618c9bc333a9c424e9e8f057fc9bf0452900
Nishant Arora <1895906+whizzzkid@users.noreply.github.com>
2023-02-03 20:28:50 -0700

    feat(sw): :sparkles: Custom Caching Strategy

--- service-worker.js
+++ service-worker.js
@@ -3,13 +3,52 @@ import { ExpirationPlugin } from 'workbox-expiration';
 import { cleanupOutdatedCaches, precacheAndRoute } from 'workbox-precaching';
 import { registerRoute, setDefaultHandler } from 'workbox-routing';
 import { CacheFirst, NetworkFirst, StaleWhileRevalidate } from 'workbox-strategies';
-import {Strategy} from 'workbox-strategies';
+import { Strategy } from 'workbox-strategies';
 
+/**
+ * This is a custom strategy to cache the milestone children of a Starmap.
+ * The benefit of writing this as a workbox strategy is we can use other workbox plugins like expiration.
+ */
 class CacheChildren extends Strategy {
-  async _handle(request, handler) {
-    const { issue_number, owner } = await request.clone().json()
 
-    return handler.fetch(request);
+  /**
+   *
+   * @param {string} cacheKey
+   * @param {object} request
+   * @param {object} handler
+   * @returns
+   */
+  async populateCacheAsync(cacheKey, request, handler) {
+    const response = await handler.fetch(request)
+    handler.cachePut(cacheKey, response.clone())
+    return response
+  }
+
+  /**
+   *
+   * @param {object} request
+   * @param {object} handler
+   * @returns
+   */
+  async _handle(request, handler) {
+    try {
+      // Cloning ensures we don't consume the request here.
+      const { issue_number, owner } = await request.clone().json()
+      // We are using the owner and issue number as the cache key.
+      const cacheKey = `child-${owner}-${issue_number}`
+      // Checking if the cache already has the response.
+      const cachedResponse = await handler.cacheMatch(cacheKey)
+      if (cachedResponse) {
+        // WARNING: We're not awaiting this call deliberately. We want to populate the cache in the background.
+        // Essentially, poor-man's version of stale-while-revalidate.
+        this.populateCacheAsync(cacheKey, request, handler)
+        return cachedResponse
+      }
+      // If the cache doesn't have the response, we populate it, and here we await the response.
+      return await this.populateCacheAsync(cacheKey, request, handler)
+    } catch (error) {
+      throw new Error(`Custom Caching of Children Failed with error: ${error}`)
+    }
   }
 }
 
@@ -77,7 +116,10 @@ registerRoute(
     plugins: [
       new ExpirationPlugin({
         ...DEFAULT_EXPIRATION_OPTIONS,
-        maxEntries: 10000
+        maxEntries: 1000,
+        matchOptions: {
+          ignoreVary: true
+        }
       }),
     ],
   }),
