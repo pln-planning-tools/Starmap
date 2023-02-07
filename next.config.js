e966ca4e69a89c8e1c4365d3c3da8fecfac34585
Nishant Arora <1895906+whizzzkid@users.noreply.github.com>
2023-02-07 15:44:28 -0700

    fix: :wastebasket: not needed

--- next.config.js
+++ next.config.js
@@ -1,5 +1,4 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-// eslint-disable-next-line @typescript-eslint/no-var-requires
 const withPWA = require('next-pwa')({
   dest: 'public',
   register: true,
