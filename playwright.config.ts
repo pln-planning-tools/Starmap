6c7838371e2c48973b72fabc1e7a6ee93fdd57c5
Nishant Arora <1895906+whizzzkid@users.noreply.github.com>
2023-02-07 20:51:33 -0700

    fix: :wrench: Fixing env variable.

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,4 +1,4 @@
-import { PlaywrightTestConfig, devices } from '@playwright/test'
+import { PlaywrightTestConfig } from '@playwright/test'
 import path from 'path'
 
 // Use process.env.PORT by default and fallback to port 3000
@@ -21,7 +21,7 @@ const config: PlaywrightTestConfig = {
   // Run your local dev server before starting the tests:
   // https://playwright.dev/docs/test-advanced#launching-a-development-web-server-during-the-tests
   webServer: {
-    command: 'yarn dev',
+    command: `PLN_ADMIN_GITHUB_TOKEN=${process.env.PLN_ADMIN_GITHUB_TOKEN} yarn dev`,
     url: baseURL,
     timeout: 120 * 1000,
     reuseExistingServer: !process.env.CI,
