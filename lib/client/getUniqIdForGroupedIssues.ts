0420d25967e919b3773b5e461fc60c5f9f3f1eaf
Nishant Arora <1895906+whizzzkid@users.noreply.github.com>
2023-02-09 21:35:05 -0700

    fix: :bulb: Adding hashCode for items that are missing somehow.

--- lib/client/getUniqIdForGroupedIssues.ts
+++ lib/client/getUniqIdForGroupedIssues.ts
@@ -4,14 +4,19 @@ import { DetailedViewGroup, IssueData } from '../types';
 interface GetUniqIdForGroupedIssuesArgs extends Omit<DetailedViewGroup, 'items'> {
   items: Pick<IssueData, 'node_id'>[]
 }
+
+// Based on Java's hashCode implementation: https://stackoverflow.com/a/7616484/104380
+const hashCode = str => [...str].reduce((hash, chr) => 0 | (31 * hash + chr.charCodeAt(0)), 0)
+
 export default function getUniqIdForGroupedIssues (groupedIssues: GetUniqIdForGroupedIssuesArgs[]): string {
   return groupedIssues.map((group) => {
     const groupChildrenId = group.items.map((item) => {
       try {
         return item.node_id;
       } catch (e) {
+        console.error('Problem getting node_id for group item:', item)
         console.error(e)
-        return '';
+        return hashCode(JSON.stringify(item));
       }
     }).join('-')
     return `${group.groupName}${groupChildrenId}`
