101d580f799e03f02bdc23695d05b8e0087fb6d3
Nishant Arora <1895906+whizzzkid@users.noreply.github.com>
2023-02-11 11:29:12 -0700

    fix: :lipstick: skeleton

--- components/roadmap-grid/today-marker-toggle.tsx
+++ components/roadmap-grid/today-marker-toggle.tsx
@@ -1,27 +1,22 @@
-import { Button, Skeleton, Text } from '@chakra-ui/react';
+import { Button, Text } from '@chakra-ui/react';
 import React from 'react';
 import SvgEyeClosedIcon from '../icons/svgr/SvgEyeClosedIcon';
 import SvgEyeOpenIcon from '../icons/svgr/SvgEyeOpenIcon';
 
 import { setShowTodayMarker, useShowTodayMarker } from '../../hooks/useShowTodayMarker';
 
-
-import { useGlobalLoadingState } from '../../hooks/useGlobalLoadingState';
 import styles from './today-marker.module.css';
 
 export function TodayMarkerToggle() {
   const showTodayMarker = useShowTodayMarker();
-  const globalLoadingState = useGlobalLoadingState();
 
   return (
-    <Skeleton isLoaded={!globalLoadingState.get()}>
-      <Button
-        className={showTodayMarker ? styles.todayMarkerToggle : styles.todayMarkerToggleDisabled}
-        onClick={() => setShowTodayMarker(!showTodayMarker)}
-      >
-        { showTodayMarker ? <SvgEyeOpenIcon /> : <SvgEyeClosedIcon /> }
-      <Text fontSize={"16px"} pl={'4px'} fontWeight={500}>Today</Text>
-      </Button>
-    </Skeleton>
+    <Button
+      className={showTodayMarker ? styles.todayMarkerToggle : styles.todayMarkerToggleDisabled}
+      onClick={() => setShowTodayMarker(!showTodayMarker)}
+    >
+      { showTodayMarker ? <SvgEyeOpenIcon /> : <SvgEyeClosedIcon /> }
+    <Text fontSize={"16px"} pl={'4px'} fontWeight={500}>Today</Text>
+    </Button>
   );
 }
