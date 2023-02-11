4d234d2f2ead78ecd5af3890344025c97d5f5be5
Nishant Arora <1895906+whizzzkid@users.noreply.github.com>
2023-02-11 10:55:08 -0700

    fix: :wastebasket: hashCode won't work because proxy is not ready yet.

--- lib/client/getUniqIdForGroupedIssues.ts
+++ lib/client/getUniqIdForGroupedIssues.ts
@@ -5,9 +5,6 @@ interface GetUniqIdForGroupedIssuesArgs extends Omit<DetailedViewGroup, 'items'>
   items: Pick<IssueData, 'node_id'>[]
 }
 
-// Based on Java's hashCode implementation: https://stackoverflow.com/a/7616484/104380
-const hashCode = str => [...str].reduce((hash, chr) => 0 | (31 * hash + chr.charCodeAt(0)), 0)
-
 export default function getUniqIdForGroupedIssues (groupedIssues: GetUniqIdForGroupedIssuesArgs[]): string {
   return groupedIssues.map((group) => {
     const groupChildrenId = group.items.map((item) => {
@@ -16,7 +13,7 @@ export default function getUniqIdForGroupedIssues (groupedIssues: GetUniqIdForGr
       } catch (e) {
         console.error('Problem getting node_id for group item:', item)
         console.error(e)
-        return hashCode(JSON.stringify(item));
+        return Date.now().toString() + Math.random().toString()
       }
     }).join('-')
     return `${group.groupName}${groupChildrenId}`
