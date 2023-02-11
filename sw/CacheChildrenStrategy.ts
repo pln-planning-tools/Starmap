c208ce6ca9d87c579a29ac9adf499fe8d43d0a8f
Nishant Arora <1895906+whizzzkid@users.noreply.github.com>
2023-02-11 10:05:28 -0700

    Saving state

--- sw/CacheChildrenStrategy.ts
+++ sw/CacheChildrenStrategy.ts
@@ -1,5 +1,5 @@
 /* eslint-disable import/no-unused-modules */
-import { Strategy } from 'workbox-strategies';
+import { Strategy, StrategyHandler } from 'workbox-strategies';
 
 /**
  * This is a custom strategy to cache the milestone children of a Starmap.
@@ -12,12 +12,10 @@ export class CacheChildren extends Strategy implements Strategy {
    * @param {string} cacheKey
    * @param {object} request
    * @param {object} handler
-   * @returns
    */
-  async populateCacheAsync(cacheKey, request, handler) {
+  async populateCacheAsync(cacheKey: string, request: Request, handler: StrategyHandler): Promise<void> {
     const response = await handler.fetch(request)
     handler.cachePut(cacheKey, response.clone())
-    return response
   }
 
   /**
@@ -26,9 +24,8 @@ export class CacheChildren extends Strategy implements Strategy {
    * @param {object} handler
    * @returns
    */
-  async _handle(request, handler) {
+  async _handle(request: Request, handler: StrategyHandler): Promise<Response | undefined> {
     try {
-
       // Cloning ensures we don't consume the request here.
       const { issue_number, owner, repo } = await request.clone().json()
       // We are using the owner, repo and issue number as the cache key.
@@ -42,7 +39,7 @@ export class CacheChildren extends Strategy implements Strategy {
         return cachedResponse
       }
       // If the cache doesn't have the response, we populate it, and here we await the response.
-      return await this.populateCacheAsync(cacheKey, request, handler)
+      await this.populateCacheAsync(cacheKey, request, handler)
     } catch (error) {
       throw new Error(`Custom Caching of Children Failed with error: ${error}`)
     }
