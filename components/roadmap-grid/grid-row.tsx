52ec2259c1ee0e78319d7b5551d96785f43a351a
Alexandre Nicastro <alexandre.nicastro@nikas.org>
2023-02-21 03:15:35 -0300

    chore: sync some files with upstream
    Signed-off-by: Alexandre Nicastro <alexandre.nicastro@nikas.org>

--- components/roadmap-grid/grid-row.tsx
+++ components/roadmap-grid/grid-row.tsx
@@ -1,20 +1,18 @@
 import NextLink from 'next/link';
-import { useRouter } from 'next/router';
-
-import { Box, Center, Flex, Text } from '@chakra-ui/react';
-import type { State } from '@hookstate/core';
-
+import { Flex, Text, Box, Center } from '@chakra-ui/react';
 import React, { useEffect, useMemo, useState } from 'react';
-import { ReactElement } from 'react-markdown/lib/react-markdown';
+import type { State } from '@hookstate/core'
 
-import { useViewMode } from '../../hooks/useViewMode';
-import { globalTimeScaler } from '../../lib/client/TimeScaler';
 import { dayjs } from '../../lib/client/dayjs';
-import { getLinkForRoadmapChild } from '../../lib/client/getLinkForRoadmapChild';
-import { paramsFromUrl } from '../../lib/paramsFromUrl';
 import { IssueData, IssueDataViewInput } from '../../lib/types';
-import { SvgGitHubLogoWithTooltip } from '../icons/svgr/SvgGitHubLogoWithTooltip';
 import styles from './Roadmap.module.css';
+import { SvgGitHubLogoWithTooltip } from '../icons/svgr/SvgGitHubLogoWithTooltip';
+import { globalTimeScaler } from '../../lib/client/TimeScaler';
+import { ReactElement } from 'react-markdown/lib/react-markdown';
+import { getLinkForRoadmapChild } from '../../lib/client/getLinkForRoadmapChild';
+import { useRouter } from 'next/router';
+import { useViewMode } from '../../hooks/useViewMode';
+import { paramsFromUrl } from '../../lib/paramsFromUrl';
 
 interface GridRowProps extends IssueDataViewInput {
   milestone: State<IssueData>;
@@ -30,30 +28,18 @@ export function GridRow({
   timelineTicks,
   numGridCols,
   numHeaderItems,
-  issueDataState,
+  issueDataState
 }: GridRowProps): ReactElement | null {
   const viewMode = useViewMode();
   const routerQuery = useRouter().query;
-  const [closestDateIdx, setClosestDateIdx] = useState(
-    Math.round(globalTimeScaler.getColumn(dayjs.utc(milestone.due_date.get()).toDate())),
-  );
+  const [closestDateIdx, setClosestDateIdx] = useState(Math.round(globalTimeScaler.getColumn(dayjs.utc(milestone.due_date.get()).toDate())));
   useEffect(() => {
     setClosestDateIdx(Math.round(globalTimeScaler.getColumn(dayjs.utc(milestone.due_date.get()).toDate())));
-    // eslint-disable-next-line react-hooks/exhaustive-deps
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [milestone.due_date, globalTimeScaler.getDomain()]);
   const span = Math.max(4, numGridCols / timelineTicks.length);
-  console.log('span:', span);
   const closest = span * (closestDateIdx - 1);
-  const childLink = useMemo(
-    () =>
-      getLinkForRoadmapChild({
-        viewMode,
-        issueData: milestone.get(),
-        query: routerQuery,
-        currentRoadmapRoot: issueDataState.value,
-      }),
-    [issueDataState.value, milestone, routerQuery, viewMode],
-  );
+  const childLink = useMemo(() => getLinkForRoadmapChild({ viewMode, issueData: milestone.get(), query: routerQuery, currentRoadmapRoot: issueDataState.value }), [issueDataState.value, milestone, routerQuery, viewMode]);
   const clickable = milestone.children.length > 0;
 
   if (milestone == null || milestone.ornull == null) {
@@ -71,11 +57,11 @@ export function GridRow({
       return null;
     }
   }
-  const gridColumnEnd = closest === span ? closest : closest - 1;
+  const gridColumnEnd = closest === span ? closest : closest - 1
 
   if (span > gridColumnEnd) {
     // TODO: Handle this error
-    console.error('Span size is greater than gridColumnEnd', milestone.get({ noproxy: true }));
+    console.error('Span size is greater than gridColumnEnd', milestone.get({ noproxy: true }))
   }
   if (closestDateIdx > numGridCols) {
     /**
@@ -83,13 +69,13 @@ export function GridRow({
      * This error is sometimes happening for milestones that are currently being used as a group item.
      * i.e. we shouldn't be attempting to render those at all.
      */
-    console.error('closestDateIdx is greater than numGridCols', milestone.get({ noproxy: true }));
+    console.error('closestDateIdx is greater than numGridCols', milestone.get({ noproxy: true }))
   }
 
   let className = '';
   try {
     const { owner, repo, issue_number } = paramsFromUrl(milestone.html_url.value);
-    className = `js-milestoneCard-${owner}-${repo}-${issue_number}`;
+    className = `js-milestoneCard-${owner}-${repo}-${issue_number}`
   } catch {}
 
   const rowItem = (
@@ -100,25 +86,22 @@ export function GridRow({
         gridColumnEnd: `${closestDateIdx}`,
         background: '#ffffff',
       }}
-      className={`${styles.item} ${styles.issueItem} ${clickable && styles.wrapperLink} js-milestoneCard ${
-        clickable && className
-      }`}
+      className={`${styles.item} ${styles.issueItem} ${clickable && styles.wrapperLink} js-milestoneCard ${clickable && className}`}
     >
-      <Flex direction={{ base: 'column' }} justify='space-between' position='relative' w='100%'>
-        <Flex direction='column' w='100%'>
-          <Text as='b' className={`${styles.milestoneTitleWrapper} ${clickable && styles.milestoneTitleWrapperLink}`}>
-            {milestone.title.value}
-          </Text>
-          <Flex h='8px' w='100%' borderRadius='20px' bgColor='#F1F4F8'>
-            <Box w={`${milestone.completion_rate.value}%`} h='100%' borderRadius='20px' bg='#7DE087' />
+      <Flex direction={{ base:"column" }} justify="space-between" position="relative" w="100%">
+        <Flex direction="column" w="100%">
+          <Text as="b" className={`${styles.milestoneTitleWrapper} ${clickable && styles.milestoneTitleWrapperLink}`}>{milestone.title.value}</Text>
+          <Flex h='8px' w="100%" borderRadius="20px" bgColor="#F1F4F8">
+            <Box w={`${milestone.completion_rate.value}%`} h="100%" borderRadius="20px" bg="#7DE087" />
           </Flex>
         </Flex>
-        <Flex flexDirection='row' align='flex-end' justify='space-between' mt={{ base: '8px' }}>
+        <Flex flexDirection="row" align="flex-end" justify="space-between" mt={{ base: "8px" }}>
           <p className={styles.milestoneDate}>{dayjs(milestone.due_date.value).format('DD-MMM-YY')}</p>
           <Center>
             <SvgGitHubLogoWithTooltip githuburl={milestone.html_url.value} />
           </Center>
         </Flex>
+
       </Flex>
     </div>
   );
