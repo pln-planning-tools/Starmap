2513ccfa2dbb5f3be68455f2700928b39592401f
Nishant Arora <1895906+whizzzkid@users.noreply.github.com>
2023-02-21 17:05:28 -0700

    fix(sw): :wastebasket: Let users refresh on their own.

--- hooks/useStarmapContentUpdated.ts
+++ hooks/useStarmapContentUpdated.ts
@@ -4,9 +4,8 @@ import Toast from 'awesome-toast-component'
 export default function useStarmapContentUpdated (): void {
   useEffect(() => {
     window.document.addEventListener('starmap:content:updated', () => {
-      new Toast('Starmap Updated! Click here to refresh the page.', {
+      new Toast('Starmap Content Changed! Please Refresh.', {
         waitForEvent: true,
-        afterHide: () => window.location.reload(),
         style: {
           container: [
             ['cursor', 'pointer'],
