83cbb5866d2915046d2cc2537bd023bb2cf270d2
Nishant Arora <1895906+whizzzkid@users.noreply.github.com>
2023-02-23 03:58:38 -0700

    feat(sw): :poop: Fixing when to show toast.

--- hooks/useStarmapContentUpdated.ts
+++ hooks/useStarmapContentUpdated.ts
@@ -1,17 +1,41 @@
-import { useEffect } from 'react';
-import Toast from 'awesome-toast-component'
+import Toast from 'awesome-toast-component';
+import { useEffect, useState } from 'react';
+import { useGlobalLoadingState } from './useGlobalLoadingState';
 
-export default function useStarmapContentUpdated (): void {
+const FIRST_LOAD_KEY_PREFIX = 'starmap:content:updated:first-load:';
+
+export default async function useStarmapUpdateAvailable(): Promise<void> {
+  const globalLoadingState = useGlobalLoadingState();
+  const [updateAvailable, setUpdateAvailable] = useState(false);
+
   useEffect(() => {
-    window.document.addEventListener('starmap:content:updated', () => {
-      new Toast('Starmap Content Changed! Please Refresh.', {
-        waitForEvent: true,
-        style: {
-          container: [
-            ['cursor', 'pointer'],
-          ]
-        }
-      })
+    window.document.addEventListener('starmap:content:updated', ({ detail }) => {
+      if (detail.cacheName === 'milestones' || detail.cacheName === 'roadmap') {
+        setUpdateAvailable(true);
+      }
     })
-  }, []);
+  });
+
+  useEffect(() => {
+    const firstLoadKey = `${FIRST_LOAD_KEY_PREFIX}${window.location.pathname}`;
+    // If this is the first time the user is loading the page, don't show the toast.
+    // Using localStorage to persist this value across page refreshes.
+    // Because localStorage is synchronous.
+    const isFirstLoad = localStorage.getItem(firstLoadKey) === null;
+    if (updateAvailable && !globalLoadingState.get()) {
+      if (isFirstLoad) {
+        localStorage.setItem(firstLoadKey, 'true');
+      } else {
+        new Toast('Starmap Content Updated! Please Refresh.', {
+          timeout: 0,
+          style: {
+            container: [
+              ['border-radius', '8px']
+            ]
+          }
+        })
+      }
+      setUpdateAvailable(false);
+    }
+  }, [updateAvailable, globalLoadingState]);
 };
