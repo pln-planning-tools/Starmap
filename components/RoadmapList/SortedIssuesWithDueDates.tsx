0f4154655b88a9f4e3e87895192ae61750bc20f4
Russell Dempsey <1173416+SgtPooki@users.noreply.github.com>
2023-02-28 10:27:50 -0800

    feat(listview): bullet icons and connectorlines

--- components/RoadmapList/SortedIssuesWithDueDates.tsx
+++ components/RoadmapList/SortedIssuesWithDueDates.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
-import { Flex, Center, Box, Spacer, Icon, Stack, Grid } from '@chakra-ui/react'
+import { Flex, Center, Box, Spacer, Icon, Stack, Grid, GridItem } from '@chakra-ui/react'
 
 import { IssueData } from '../../lib/types';
+import BulletIcon from './BulletIcon';
+import BulletConnector from './BulletConnector';
 
 interface SortedIssuesWithDueDatesProps {
   issuesWithDueDates: IssueData[]
@@ -16,14 +18,15 @@ const OpenCircleIcon = (props) => (
       d='M 100, 100 m -75, 0 a 75,75 0 1,0 150,0 a 75,75 0 1,0 -150,0'
     />
     // Do a new path around the outside of the existing circle in a dark gray
-    <path
+    {/* <path
       fillOpacity={1}
-      stroke='gray'
+      // stroke='gray'
       strokeWidth={2}
       d='M 100, 100 m -75, 0 a 75,75 0 1,0 150,0 a 75,75 0 1,0 -150,0'
-    />
+    /> */}
   </Icon>
 )
+
 
 const ClosedCircleIcon = (props) => (
   <Icon viewBox='0 0 200 200' {...props}>
@@ -31,37 +34,66 @@ const ClosedCircleIcon = (props) => (
       fill='currentColor'
       d='M 100, 100 m -75, 0 a 75,75 0 1,0 150,0 a 75,75 0 1,0 -150,0'
     />
-    <path
+    {/* <path
       fill='gray'
-      fillOpacity={1}
+      // fillOpacity={1}
       stroke='gray'
       strokeWidth={2}
       d='M 110, 110 m -75, 0 a 70,70 0 1,0 150,0 a 75,75 0 1,0 -150,0'
-    />
+    /> */}
   </Icon>
 )
 
+
 export default function SortedIssuesWithDueDates({ issuesWithDueDates }: SortedIssuesWithDueDatesProps): JSX.Element {
+  const totalColumns = 5
+  const dateColumns = totalColumns/5
+  const circleColumns = 1
+  const descriptionColStart = dateColumns + circleColumns + 1
+
   return (
-    <Stack width="100%" direction={"column"} spacing="10" alignItems="center">
-      {issuesWithDueDates.map((issue) => (
+    <Grid width="100%"
+      // templateAreas={`"date icon title"
+      //                 "empty empty description"`}
+      // gridTemplateRows={'1fr 1fr 1fr'}
+      // gridTemplateColumns={'1fr 0.5fr 4fr'}
+      templateRows={`repeat(${issuesWithDueDates.length}, 1fr)`}
+      // templateColumns={`repeat(${totalColumns}, 1fr)`}
+      alignItems="center">
+      {issuesWithDueDates.map((issue, index) => (
         <>
-        <Stack width="100%" key={issue.html_url} direction="row" spacing="5" alignItems="center" justifyContent="flex-start">
+        {/* <Grid width="100%" key={issue.html_url} direction="row" spacing="5" alignItems="center" justifyContent="flex-start"> */}
+        <Grid width="100%" key={issue.html_url}
+          templateAreas={`"date icon title"
+                          "empty line description"`}
+          gridTemplateRows={'1fr 1fr'}
+          gridTemplateColumns={'1fr 0.5fr 8fr'}
+          >
           {/* <article> */}
-            <Box>{issue.due_date}</Box>
+            <GridItem area="date"><Center>{issue.due_date}</Center></GridItem>
             {/* <Spacer/> */}
-            <Box>{ issue.completion_rate === 100 ? <ClosedCircleIcon/> : <OpenCircleIcon />}</Box>
+            <GridItem area="icon">
+              <Center>
+                <BulletIcon completion_rate={issue.completion_rate} />
+              </Center>
+            </GridItem>
             {/* <Spacer/> */}
-            <Box>{issue.title}</Box>
+            <GridItem area="title">{issue.title}</GridItem>
+            <GridItem area="line" pos="relative">
+              <Center>
+              <BulletConnector isLast={index === issuesWithDueDates.length -1}>&nbsp;</BulletConnector>
+              </Center>
+            </GridItem>
+
           {/* </article> */}
-        </Stack>
-        <Stack width="100%" key={issue.html_url} direction="row" spacing="5">
-          <Spacer/>
-          <Spacer/>
-          <Box>{issue.description}</Box>
-        </Stack>
+        {/* </Grid>
+        <Grid width="100%" key={issue.html_url} direction="row" spacing="5"> */}
+          {/* <Spacer/>
+          <Spacer/> */}
+          <GridItem area="description">description: {issue.description}</GridItem>
+        </Grid>
         </>
       ))}
-    </Stack>
+    </Grid>
   )
 }
--- components/RoadmapList/index.tsx
+++ components/RoadmapList/index.tsx
@@ -1,4 +1,4 @@
-// import React from 'react';
+import React from 'react';
 import { IssueDataViewInput } from '../../lib/types';
 import SortedIssuesWithDueDates from './SortedIssuesWithDueDates';
 
