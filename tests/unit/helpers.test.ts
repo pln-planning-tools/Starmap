def13faa093e233cb1ca0c829aae4e1ff5dedfba
Russell Dempsey <1173416+SgtPooki@users.noreply.github.com>
2023-04-24 15:21:08 -0700

    feat: add ETA format deprecation warnings (#312)
    * feat: add ETA format deprecation warnings
    
    fixes https://github.com/pln-planning-tools/Starmap/issues/311
    
    * fix: accept space in front of eta: key
    
    * fix: ensure errorManager context is kept

--- tests/unit/helpers.test.ts
+++ tests/unit/helpers.test.ts
@@ -1,28 +1,48 @@
 import { getEtaDate } from '../../lib/helpers';
 
 describe('getEtaDate', () => {
+  const config = { addError: jest.fn(), issue: { html_url: 'test', title: 'test' } }
+  beforeEach(function () {
+    jest.resetAllMocks()
+  })
+  function ensureDeprecationError(callNumber: number) {
+
+    expect(config.addError).toHaveBeenCalledTimes(callNumber)
+    expect(config.addError.mock.calls[callNumber-1][0]).toHaveProperty('errorTitle', 'ETA format deprecated');
+    expect(config.addError.mock.calls[callNumber-1][0]).toHaveProperty('errorMessage', 'ETA must use ISO 8601 standard (YYYY-MM-DD). Please see https://github.com/pln-planning-tools/Starmap/issues/275');
+  }
+
   it('should return a date string for YYYY-MM-DD', () => {
     // dates
-    expect(getEtaDate('eta:2022-11-14')).toBe('2022-11-14');
-    expect(getEtaDate('eta : 2022-11-14')).toBe('2022-11-14');
-    expect(getEtaDate('eta: 2022-11-14')).toBe('2022-11-14');
+    expect(getEtaDate('eta:2022-11-14', config)).toBe('2022-11-14');
+    expect(config.addError).toHaveBeenCalledTimes(0)
+    expect(getEtaDate('eta : 2022-11-14', config)).toBe('2022-11-14');
+    expect(config.addError).toHaveBeenCalledTimes(0)
+    expect(getEtaDate('eta: 2022-11-14', config)).toBe('2022-11-14');
+    expect(config.addError).toHaveBeenCalledTimes(0)
+    expect(getEtaDate('ETA: 2022-11-14', config)).toBe('2022-11-14');
+    expect(config.addError).toHaveBeenCalledTimes(0)
   });
 
   it('should return a date string for quarters and optional years', () => {
     // quarters
-    expect(getEtaDate('eta: 2022Q4')).toBe('2022-12-31');
-    expect(getEtaDate('eta: 2024Q3')).toBe('2024-09-30');
+    expect(getEtaDate('eta: 2022Q4', config)).toBe('2022-12-31');
+    ensureDeprecationError(1)
+    expect(getEtaDate('eta: 2024Q3', config)).toBe('2024-09-30');
+    ensureDeprecationError(2)
   });
 
   it('should return a date string for months and optional years', () => {
     // months + years
-    expect(getEtaDate('eta: 2022-12')).toBe('2022-12-31');
-    expect(getEtaDate('eta: 2024-07')).toBe('2024-07-31');
+    expect(getEtaDate('eta: 2022-12', config)).toBe('2022-12-31');
+    ensureDeprecationError(1)
+    expect(getEtaDate('eta: 2024-07', config)).toBe('2024-07-31');
+    ensureDeprecationError(2)
   });
 
   it('should throw error when date is malformed.', () => {
     // invalid dates
-    expect(() => getEtaDate('eta: 14-11-2022')).toThrow(); // dd-mm-yyyy
-    expect(() => getEtaDate('eta: ')).toThrow(); // empty
+    expect(() => getEtaDate('eta: 14-11-2022', config)).toThrow(); // dd-mm-yyyy
+    expect(() => getEtaDate('eta: ', config)).toThrow(); // empty
   });
 });
